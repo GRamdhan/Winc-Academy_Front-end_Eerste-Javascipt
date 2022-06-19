1f82523dee5b77fd634cc74929f21039e84a17d4
GRamdhan <73478034+GRamdhan@users.noreply.github.com>
2022-06-19 15:53:44 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+console.log("Hello Winc Academy");
+let name = "Ganesh";
+console.log(name);
+// Let 'if' is not possible, 'if' is already in use 
+console.log(4 + 4);
+console.log("4" + "4"); // gives 44 as result
+console.log(2 + 2);
+console.log(2 - 2);
+console.log(2 * 2);
+console.log(20 % 2);
+// let age = 42;
+// console.log(typeof age);
+let age = "42";
+console.log(typeof age);
